253e41a778284ddea7a2dfa2927aa01b5873b290
bnp800 <bnp800@github.com>
2020-10-22 15:09:32 +0300

    Update babele-register.js

--- pf1e_cn/babele-register.js
+++ pf1e_cn/babele-register.js
@@ -2,9 +2,9 @@ Hooks.on('init', () => {
 
 	if(typeof Babele !== 'undefined') {
 		Babele.get().register({
-			module: 'pf1e_cn',
+			module: 'pf1e_cn_compendium',
 			lang: 'cn',
 			dir: 'compendium'
-		}); 
+		});
 	}
 });
